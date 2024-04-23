318eb60adf5b9795438b98956557f469d2e0ebc5
Zhe Ying <83914599+Pikaciu@users.noreply.github.com>
2024-04-23 16:27:38 +0800

    Update test_search.py

--- Selenium/test_search.py
+++ Selenium/test_search.py
@@ -6,7 +6,6 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
@@ -19,8 +18,7 @@ class TestSearch():
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless")
-    service = Service('/home/zheying/chromedriver')
-    self.driver = webdriver.Chrome(options=options, service=service)
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
   def teardown_method(self, method):
